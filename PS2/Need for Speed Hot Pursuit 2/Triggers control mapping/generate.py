eecef0ffdf4d3f24274efae9d6f85d6d941f33d0
Silent <zdanio95@gmail.com>
2024-04-13 14:41:48 +0200

    NFS HP2 PS2: Dumped alpha controller configs

--- PS2/Need for Speed Hot Pursuit 2/Triggers control mapping/generate.py
+++ PS2/Need for Speed Hot Pursuit 2/Triggers control mapping/generate.py
@@ -1,9 +1,60 @@
+import binascii
 import json
 import mmap
 import struct
+from bidict import bidict
+from collections import namedtuple
+
+def getVersionData(elf):
+    hash = binascii.crc32(elf) & 0xFFFFFFFF
+    VersionData = namedtuple('VersionData', ['eventNames', 'numEventNames',
+                             'scannerConfigs', 'numScannerConfigsPtr', 'scanners'])
+
+    data = {
+        # NTSC-U
+        0xb879bb85 : VersionData(
+            eventNames=0x2FA2D0, numEventNames=130,
+            scannerConfigs=0x2DC500, numScannerConfigsPtr=0x32FBE0,
+        scanners=bidict({
+            0x1545E8 : 'TypeChanged',
+            0x1546B0 : 'DigitalDown',
+            0x154A90 : 'DigitalRepeat',
+            0x1547E8 : 'DigitalUpOrDown',
+            0x154C90 : 'DigitalAnalog',
+            0x154628 : 'DigitalAnyButton',
+            0x154E48 : 'Analog',
+            0x154918 : 'DigitalDoublePress',
+            0x154BE8 : 'DigitalSteer',
+        })),
+
+        # NTSC-U A1.56 prototype
+        0x6297bb64 : VersionData(
+            eventNames=0x2F1B48, numEventNames=130,
+            scannerConfigs=0x2D3AC0, numScannerConfigsPtr=0x326C40,
+        scanners=bidict({
+            0x1541A0 : 'TypeChanged',
+            0x1541E0 : 'DigitalAnyButton',
+            0x154268 : 'DigitalDown',
+            0x154380 : 'DigitalDownPlus',
+            0x1543A0 : 'DigitalUpOrDown',
+            0x1544B0 : 'DigitalUpOrDownPlus',
+            0x1544D0 : 'DigitalDoublePress',
+            0x154648 : 'DigitalRepeat',
+            0x1547A0 : 'DigitalSteer',
+            0x154848 : 'DigitalAnalog',
+            0x154A00 : 'Analog',
+        })),
+    }
+    return data[hash]
+
+def readU32(buf, addr):
+    return struct.unpack_from('<I', buf, addr)[0]
 
 def readCString(buf, addr, encoding='ascii'):
     return buf[addr:].split(b'\x00', 1)[0].decode(encoding)
+
+def vaddrToOffset(vaddr):
+    return vaddr - 0x100000 + 0x1000
 
 def openFileMapping(path):
     with open(path) as f:
@@ -12,21 +63,19 @@ def openFileMapping(path):
 def dumpEventNames(elfPath, output):
     events = []
     with openFileMapping(elfPath) as elf:
+        VERSION_DATA = getVersionData(elf)
+
         FORMAT = '<II'
         ENTRY_SIZE = struct.calcsize(FORMAT)
-        NUM_ENTRIES = 130
 
-        def vaddrToOffset(vaddr):
-            return vaddr - 0x100000 + 0x1000
-
-        START_OFFSET = 0x1FB2D0
-        for entry in struct.iter_unpack(FORMAT, elf[START_OFFSET:START_OFFSET+(ENTRY_SIZE*NUM_ENTRIES)]):
+        startOffset = vaddrToOffset(VERSION_DATA.eventNames)
+        for entry in struct.iter_unpack(FORMAT, elf[startOffset:startOffset+(ENTRY_SIZE*VERSION_DATA.numEventNames)]):
             events.append({'id': entry[0], 'name': readCString(elf, vaddrToOffset(entry[1]))})
 
     with open(output, 'w') as f:
         json.dump(events, f, indent=2)
 
-def dumpScannerConfigs(elfPath, eventNamesPath, numConfigs, output):
+def dumpScannerConfigs(elfPath, eventNamesPath, output):
     scannerConfigs = []
 
     def getEventNames():
@@ -37,26 +86,15 @@ def dumpScannerConfigs(elfPath, eventNamesPath, numConfigs, output):
         return result
 
     with openFileMapping(elfPath) as elf:
+        VERSION_DATA = getVersionData(elf)
+
         FORMAT = '<8BII2B2BbBH2II'
         ENTRY_SIZE = struct.calcsize(FORMAT)
         EVENT_NAMES = getEventNames()
-        CALLBACKS = {
-            0x1545E8 : 'TypeChanged',
-            0x1546B0 : 'DigitalDown',
-            0x154A90 : 'DigitalRepeat',
-            0x1547E8 : 'DigitalUpOrDown',
-            0x154C90 : 'DigitalAnalog',
-            0x154628 : 'DigitalAnyButton',
-            0x154E48 : 'Analog',
-            0x154918 : 'DigitalDoublePress',
-            0x154BE8 : 'DigitalSteer',
-        }
 
-        def vaddrToOffset(vaddr):
-            return vaddr - 0x100000 + 0x1000
-
-        START_OFFSET = 0x1DD500
-        for entry in struct.iter_unpack(FORMAT, elf[START_OFFSET:START_OFFSET+(ENTRY_SIZE*numConfigs)]):
+        startOffset = vaddrToOffset(VERSION_DATA.scannerConfigs)
+        numScannerConfigs = readU32(elf, vaddrToOffset(VERSION_DATA.numScannerConfigsPtr))
+        for entry in struct.iter_unpack(FORMAT, elf[startOffset:startOffset+(ENTRY_SIZE*numScannerConfigs)]):
             def trimZeroes(arr):
                 while len(arr) > 0 and arr[-1] == 0:
                     arr = arr[:-1]
@@ -66,7 +104,7 @@ def dumpScannerConfigs(elfPath, eventNamesPath, numConfigs, output):
             newEntry['configs'] = trimZeroes(entry[:8])
             newEntry['eventName'] = EVENT_NAMES.get(entry[8], f'JOY_EVENT_UNK_{entry[8]}')
             if entry[9] != 0:
-                newEntry['scanner'] = CALLBACKS[entry[9]]
+                newEntry['scanner'] = VERSION_DATA.scanners[entry[9]]
             newEntry['xor'] = entry[10:12],
             newEntry['index'] = [(x >> 5) for x in entry[12:14]]
             newEntry['shift'] = [(x & 0x1F) for x in entry[12:14]]
@@ -94,7 +132,6 @@ if __name__ == "__main__":
 
     parser.add_argument('--dump-event-names', type=str, dest='dump_event_names', help='dump event names to a JSON file')
     parser.add_argument('--dump-scanner-configs', type=str, dest='dump_scanner_configs', help='dump scanner configs to a JSON file')
-    parser.add_argument('--num-scanner-configs', type=int, dest='num_scanner_configs', help='number of scanner configs to dump')
 
     args = parser.parse_args()
 
@@ -102,4 +139,4 @@ if __name__ == "__main__":
         dumpEventNames(args.elf, args.dump_event_names)
 
     if args.dump_scanner_configs:
-        dumpScannerConfigs(args.elf, args.event_names, args.num_scanner_configs, args.dump_scanner_configs)
+        dumpScannerConfigs(args.elf, args.event_names, args.dump_scanner_configs)
