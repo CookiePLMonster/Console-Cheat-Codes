e6841d8611c308e7881ab1c687818fbae97f21d2
Silent <zdanio95@gmail.com>
2024-04-13 17:56:25 +0200

    NFS HP2 PS2: Autogenerate a patch file

--- PS2/Need for Speed Hot Pursuit 2/Triggers control mapping/generate.py
+++ PS2/Need for Speed Hot Pursuit 2/Triggers control mapping/generate.py
@@ -1,9 +1,13 @@
 import binascii
 import json
 import mmap
+import re
 import struct
+import sys
 from bidict import bidict
 from collections import namedtuple
+
+SCANNER_CONFIG_FORMAT = '<8BII2B2BbBH2II'
 
 def getVersionData(elf):
     hash = binascii.crc32(elf) & 0xFFFFFFFF
@@ -56,6 +60,11 @@ def readCString(buf, addr, encoding='ascii'):
 def vaddrToOffset(vaddr):
     return vaddr - 0x100000 + 0x1000
 
+def padList(l, length):
+    extra_length = length - len(l)
+    l.extend(0 for _ in range(extra_length))
+    return l
+
 def openFileMapping(path):
     with open(path) as f:
         return mmap.mmap(f.fileno(), 0, None, mmap.ACCESS_READ)
@@ -88,13 +97,12 @@ def dumpScannerConfigs(elfPath, eventNamesPath, output):
     with openFileMapping(elfPath) as elf:
         VERSION_DATA = getVersionData(elf)
 
-        FORMAT = '<8BII2B2BbBH2II'
-        ENTRY_SIZE = struct.calcsize(FORMAT)
+        ENTRY_SIZE = struct.calcsize(SCANNER_CONFIG_FORMAT)
         EVENT_NAMES = getEventNames()
 
         startOffset = vaddrToOffset(VERSION_DATA.scannerConfigs)
         numScannerConfigs = readU32(elf, vaddrToOffset(VERSION_DATA.numScannerConfigsPtr))
-        for entry in struct.iter_unpack(FORMAT, elf[startOffset:startOffset+(ENTRY_SIZE*numScannerConfigs)]):
+        for entry in struct.iter_unpack(SCANNER_CONFIG_FORMAT, elf[startOffset:startOffset+(ENTRY_SIZE*numScannerConfigs)]):
             def trimZeroes(arr):
                 while len(arr) > 0 and arr[-1] == 0:
                     arr = arr[:-1]
@@ -105,7 +113,7 @@ def dumpScannerConfigs(elfPath, eventNamesPath, output):
             newEntry['eventName'] = EVENT_NAMES.get(entry[8], f'JOY_EVENT_UNK_{entry[8]}')
             if entry[9] != 0:
                 newEntry['scanner'] = VERSION_DATA.scanners[entry[9]]
-            newEntry['xor'] = entry[10:12],
+            newEntry['xor'] = entry[10:12]
             newEntry['index'] = [(x >> 5) for x in entry[12:14]]
             newEntry['shift'] = [(x & 0x1F) for x in entry[12:14]]
             newEntry['invert'] = entry[14]
@@ -122,6 +130,66 @@ def dumpScannerConfigs(elfPath, eventNamesPath, output):
     with open(output, 'w') as f:
         json.dump(scannerConfigs, f, indent=2)
 
+def generatePnachFile(elfPath, eventNamesPath, scannerConfigsPath, output):
+
+    def getEventIds():
+        result = {}
+        with open(eventNamesPath) as f:
+            for event in json.load(f):
+                result[event['name']] = event['id']
+        return result
+
+    def getScannerConfigs():
+        result = []
+        with open(scannerConfigsPath) as f:
+            result = json.load(f)
+        return result
+
+    with openFileMapping(elfPath) as elf:
+        VERSION_DATA = getVersionData(elf)
+        ENTRY_SIZE = struct.calcsize(SCANNER_CONFIG_FORMAT)
+        NEW_CONFIGS = getScannerConfigs()
+
+        startOffset = VERSION_DATA.scannerConfigs
+        origNumScannerConfigs = readU32(elf, vaddrToOffset(VERSION_DATA.numScannerConfigsPtr))
+        if len(NEW_CONFIGS) > origNumScannerConfigs:
+            sys.exit('Out of space for scanner configs!')
+
+    with open(output, 'w') as pnach:
+        EVENT_IDS = getEventIds()
+        def getKeyEventId(key):
+            match = re.match(r'JOY_EVENT_UNK_(\d+)', key)
+            if match:
+                return int(match.group(1))
+            return EVENT_IDS[key]
+
+
+        LINE_TEMPLATE = 'patch=0,EE,20{0:X},extended,{1:X}\n'
+        if origNumScannerConfigs != len(NEW_CONFIGS):
+            pnach.write(LINE_TEMPLATE.format(VERSION_DATA.numScannerConfigsPtr, len(NEW_CONFIGS)))
+        for config in NEW_CONFIGS:
+            buf = struct.pack(SCANNER_CONFIG_FORMAT,
+                              *padList(config['configs'], 8),
+                              getKeyEventId(config['eventName']),
+                              VERSION_DATA.scanners.inverse.get(config.get('scanner'), 0),
+                              *config['xor'],
+                              config['index'][0] << 5 | (config['shift'][0] & 0x1F),
+                              config['index'][1] << 5 | (config['shift'][1] & 0x1F),
+                              config['invert'], config['graph'], config['unk16'],
+                              *padList(config['glyph'], 2),
+                              0 # We don't care about 'button'
+            )
+
+            # Unpack again to integers so we can generate a pnach
+            patchedMem = struct.unpack('<9I', buf)
+            offset = startOffset
+            for i in range(8):
+                pnach.write(LINE_TEMPLATE.format(offset, patchedMem[i]))
+                offset += 4
+
+            startOffset += ENTRY_SIZE
+
+
 if __name__ == "__main__":
     import argparse
 
@@ -132,6 +200,7 @@ if __name__ == "__main__":
 
     parser.add_argument('--dump-event-names', type=str, dest='dump_event_names', help='dump event names to a JSON file')
     parser.add_argument('--dump-scanner-configs', type=str, dest='dump_scanner_configs', help='dump scanner configs to a JSON file')
+    parser.add_argument('--generate-pnach', type=str, dest='generate_pnach', help='generate a patch file with new scanner configs')
 
     args = parser.parse_args()
 
@@ -140,3 +209,6 @@ if __name__ == "__main__":
 
     if args.dump_scanner_configs:
         dumpScannerConfigs(args.elf, args.event_names, args.dump_scanner_configs)
+
+    if args.generate_pnach:
+        generatePnachFile(args.elf, args.event_names, args.scanner_configs, args.generate_pnach)
