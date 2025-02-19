f5f6f087660e6749d812615c4e62ea927be0fb33
Silent <zdanio95@gmail.com>
2025-02-19 20:44:15 +0100

    NFS UG1: Ported cheats to JP and KO versions, fixed the Controller screen for Type 6 bindings

--- PS2/Tools/nfs-scanner-configs.py
+++ PS2/Tools/nfs-scanner-configs.py
@@ -6,6 +6,9 @@ import struct
 from abc import ABC, abstractmethod
 from bidict import bidict
 
+PNACH_LINE_TEMPLATE = 'patch=0,EE,{0:X},extended,{1:X}\n'
+PNACH_EXTENDED_WORD_TAG = 0x20000000
+
 def readU32(buf, addr):
     return struct.unpack_from('<I', buf, addr)[0]
 
@@ -24,10 +27,6 @@ def trimZeroes(arr):
     while len(arr) > 0 and arr[-1] == 0:
         arr = arr[:-1]
     return arr
-
-def openFileMapping(path):
-    with open(path) as f:
-        return mmap.mmap(f.fileno(), 0, None, mmap.ACCESS_READ)
 
 # Parsers
 class ShaderParserBase(ABC):
@@ -91,6 +90,10 @@ class ScannerParserHP2(ShaderParserBase):
 class ScannerParserUG1(ShaderParserBase):
     configFormat = '<8bII2B2BbBHII'
 
+    def __init__(self, eventNamesPtr, numEventNames, scannerConfigsPtr, numScannerConfigsPtr, scanners, buttonActionHashesPtr):
+        super().__init__(eventNamesPtr, numEventNames, scannerConfigsPtr, numScannerConfigsPtr, scanners)
+        self.buttonActionHashesPtr = buttonActionHashesPtr
+
     def unpack(self, elf, eventNames, entry):
         newEntry = {}
         newEntry['configs'] = trimZeroes(entry[:8])
@@ -152,7 +155,7 @@ def getParserForElf(elf):
                 0x2D58C8 : 'DigitalAnalogDown',
                 0x2D5A40 : 'DigitalAnalogUpOrDown',
                 0x2D5B68 : 'Analog'
-        }))
+        }), buttonActionHashesPtr=0x4CB988)
 
         data = {
             # Hot Pursuit 2 NTSC-U
@@ -196,6 +199,26 @@ def getParserForElf(elf):
             0xc1faa5d5 : UG1_PAL_NTSCU,
 
             # Underground NTSC-J (EA Best Hits)
+            0x763e9fdd : ScannerParserUG1(
+                eventNamesPtr=0x4E68F0, numEventNames=169,
+                scannerConfigsPtr=0x44D400, numScannerConfigsPtr=0x504A28,
+            scanners=bidict({
+                0x2D56B0 : 'TypeChanged',
+                0x2D56F0 : 'DigitalAnyButton',
+                0x2D5778 : 'DigitalDown',
+                0x2D5890 : 'DigitalDownPlus',
+                0x2D58B0 : 'DigitalUpOrDown',
+                0x2D59C0 : 'DigitalUpOrDownPlus',
+                0x2D59E0 : 'DigitalDoublePress',
+                0x2D5B58 : 'DigitalRepeat',
+                0x2D5CB0 : 'DigitalSteer',
+                0x2D5D58 : 'DigitalAnalog',
+                0x2D5FA8 : 'DigitalAnalogDown',
+                0x2D6120 : 'DigitalAnalogUpOrDown',
+                0x2D6248 : 'Analog'
+            }), buttonActionHashesPtr=0x4CC988),
+
+            # Underground NTSC-J (EA Best Hits)
             0xad838821 : ScannerParserUG1(
                 eventNamesPtr=0x4EE500, numEventNames=169,
                 scannerConfigsPtr=0x453100, numScannerConfigsPtr=0x50D0A8,
@@ -213,125 +236,182 @@ def getParserForElf(elf):
                 0x2DBF10 : 'DigitalAnalogDown',
                 0x2DC088 : 'DigitalAnalogUpOrDown',
                 0x2DC1B0 : 'Analog'
-            })),
+            }), buttonActionHashesPtr=0x4D7CF0),
+
+            # Underground NTSC-K
+            0xe6725a8c : ScannerParserUG1(
+                eventNamesPtr=0x4ED320, numEventNames=169,
+                scannerConfigsPtr=0x451F00, numScannerConfigsPtr=0x50BDA8,
+            scanners=bidict({
+                0x2DA018 : 'TypeChanged',
+                0x2DA058 : 'DigitalAnyButton',
+                0x2DA0E0 : 'DigitalDown',
+                0x2DA1F8 : 'DigitalDownPlus',
+                0x2DA218 : 'DigitalUpOrDown',
+                0x2DA328 : 'DigitalUpOrDownPlus',
+                0x2DA348 : 'DigitalDoublePress',
+                0x2DA4C0 : 'DigitalRepeat',
+                0x2DA618 : 'DigitalSteer',
+                0x2DA6C0 : 'DigitalAnalog',
+                0x2DA910 : 'DigitalAnalogDown',
+                0x2DAA88 : 'DigitalAnalogUpOrDown',
+                0x2DABB0 : 'Analog'
+            }), buttonActionHashesPtr=0x4D6B08),
         }
         return data[hash]
     except KeyError as e:
         e.add_note(f'Unknown game specified! ELF CRC32: {hash:08X}')
         raise
 
-def dumpEventNames(elfPath, output):
+def dumpEventNames(parser, output):
+    FORMAT = '<II'
+    entrySize = struct.calcsize(FORMAT)
+
+    startOffset = vaddrToOffset(parser.eventNamesPtr)
+
     events = []
-    with openFileMapping(elfPath) as elf:
-        parser = getParserForElf(elf)
-
-        FORMAT = '<II'
-        entrySize = struct.calcsize(FORMAT)
-
-        startOffset = vaddrToOffset(parser.eventNamesPtr)
-        for entry in struct.iter_unpack(FORMAT, elf[startOffset:startOffset+(entrySize*parser.numEventNames)]):
-            events.append({'id': entry[0], 'name': readCString(elf, vaddrToOffset(entry[1]))})
+    for entry in struct.iter_unpack(FORMAT, parser.elf[startOffset:startOffset+(entrySize*parser.numEventNames)]):
+        events.append({'id': entry[0], 'name': readCString(parser.elf, vaddrToOffset(entry[1]))})
 
     with open(output, 'w') as f:
         json.dump(events, f, indent=2)
 
-def dumpScannerConfigs(elfPath, eventNamesPath, output):
-    scannerConfigs = []
+def dumpScannerConfigs(parser, eventNamesPath, output):
 
     def getEventNames():
         result = {}
-        with open(eventNamesPath) as f:
+        with open(eventNamesPath, 'r') as f:
             for event in json.load(f):
                 result[event['id']] = event['name']
         return result
 
-    with openFileMapping(elfPath) as elf:
-        parser = getParserForElf(elf)
+    entrySize = struct.calcsize(parser.configFormat)
+    eventNames = getEventNames()
 
-        entrySize = struct.calcsize(parser.configFormat)
-        eventNames = getEventNames()
+    startOffset = vaddrToOffset(parser.scannerConfigsPtr)
+    numScannerConfigs = readU32(parser.elf, vaddrToOffset(parser.numScannerConfigsPtr))
 
-        startOffset = vaddrToOffset(parser.scannerConfigsPtr)
-        numScannerConfigs = readU32(elf, vaddrToOffset(parser.numScannerConfigsPtr))
-        for entry in struct.iter_unpack(parser.configFormat, elf[startOffset:startOffset+(entrySize*numScannerConfigs)]):
-            scannerConfigs.append(parser.unpack(elf, eventNames, entry))
+    scannerConfigs = []
+    for entry in struct.iter_unpack(parser.configFormat, parser.elf[startOffset:startOffset+(entrySize*numScannerConfigs)]):
+        scannerConfigs.append(parser.unpack(parser.elf, eventNames, entry))
 
     with open(output, 'w') as f:
         json.dump(scannerConfigs, f, indent=2)
 
-def generatePnachFile(elfPath, eventNamesPath, scannerConfigsPath, output):
+def dumpButtonActionHashes(parser, numHashes, output):
+    hashes = []
+    startOffset = vaddrToOffset(parser.buttonActionHashesPtr)
+    for entry in struct.iter_unpack('<I', parser.elf[startOffset:startOffset+(4*numHashes)]):
+        hashes.extend(entry)
+
+    with open(output, 'w') as f:
+        json.dump(hashes, f, indent=2)
+
+def patchScannerConfigs(parser, eventNamesPath, scannerConfigsPath, output):
 
     def getEventIds():
         result = {}
-        with open(eventNamesPath) as f:
+        with open(eventNamesPath, 'r') as f:
             for event in json.load(f):
                 result[event['name']] = event['id']
         return result
 
     def getScannerConfigs():
         result = []
-        with open(scannerConfigsPath) as f:
+        with open(scannerConfigsPath, 'r') as f:
             result = json.load(f)
         return result
 
-    with openFileMapping(elfPath) as elf:
-        parser = getParserForElf(elf)
-        entrySize = struct.calcsize(parser.configFormat)
-        NEW_CONFIGS = getScannerConfigs()
+    entrySize = struct.calcsize(parser.configFormat)
+    NEW_CONFIGS = getScannerConfigs()
 
-        startOffset = parser.scannerConfigsPtr
-        origNumScannerConfigs = readU32(elf, vaddrToOffset(parser.numScannerConfigsPtr))
-        if len(NEW_CONFIGS) > origNumScannerConfigs:
-            raise ValueError(f'Out of space for scanner configs! Specified {len(NEW_CONFIGS)} configs, max {origNumScannerConfigs}')
+    startOffset = parser.scannerConfigsPtr
+    origNumScannerConfigs = readU32(parser.elf, vaddrToOffset(parser.numScannerConfigsPtr))
+    if len(NEW_CONFIGS) > origNumScannerConfigs:
+        raise ValueError(f'Out of space for scanner configs! Specified {len(NEW_CONFIGS)} configs, max {origNumScannerConfigs}')
 
-        with open(output, 'w') as pnach:
-            EVENT_IDS = getEventIds()
-            def getKeyEventId(key):
-                match = re.match(r'JOY_EVENT_UNK_(\d+)', key)
-                if match:
-                    return int(match.group(1))
-                return EVENT_IDS[key]
+    with open(output, 'w') as pnach:
+        EVENT_IDS = getEventIds()
+        def getKeyEventId(key):
+            match = re.match(r'JOY_EVENT_UNK_(\d+)', key)
+            if match:
+                return int(match.group(1))
+            return EVENT_IDS[key]
 
-            LINE_TEMPLATE = 'patch=0,EE,{0:X},extended,{1:X}\n'
-            if origNumScannerConfigs != len(NEW_CONFIGS):
-                pnach.write(LINE_TEMPLATE.format(parser.numScannerConfigsPtr, len(NEW_CONFIGS)))
-            for config in NEW_CONFIGS:
+        if origNumScannerConfigs != len(NEW_CONFIGS):
+            pnach.write(PNACH_LINE_TEMPLATE.format(parser.numScannerConfigsPtr | PNACH_EXTENDED_WORD_TAG, len(NEW_CONFIGS)))
+        for config in NEW_CONFIGS:
+            # Pack and unpack again to integers so we can generate a pnach
+            patchedMem = struct.unpack(f'<{entrySize // 4}I', parser.pack(getKeyEventId, config))
+            offset = startOffset
+            for index, value in enumerate(patchedMem):
+                if parser.isWordRelevant(index) and readU32(parser.elf, vaddrToOffset(offset)) != value:
+                    pnach.write(PNACH_LINE_TEMPLATE.format(offset | PNACH_EXTENDED_WORD_TAG, value))
+                offset += 4
 
-                # Pack and unpack again to integers so we can generate a pnach
-                patchedMem = struct.unpack(f'<{entrySize // 4}I', parser.pack(getKeyEventId, config))
-                offset = startOffset
-                for index, value in enumerate(patchedMem):
-                    if parser.isWordRelevant(index) and readU32(elf, vaddrToOffset(offset)) != value:
-                        pnach.write(LINE_TEMPLATE.format(offset | 0x20000000, value))
-                    offset += 4
+            startOffset += entrySize
 
-                startOffset += entrySize
+def patchButtonActionHashes(parser, input, output):
+    with open(input, 'r') as f:
+        buttonActions = json.load(f)
+
+    with open(output, 'w') as pnach:
+        offset = parser.buttonActionHashesPtr
+        for action in buttonActions:
+            if readU32(parser.elf, vaddrToOffset(offset)) != action:
+                pnach.write(PNACH_LINE_TEMPLATE.format(offset | PNACH_EXTENDED_WORD_TAG, action))
+            offset += 4
 
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description='NFS Scanner Config Tool.')
-    parser.add_argument('--elf', type=str, help='path to game elf', metavar='ELF_PATH', required=True)
-    parser.add_argument('--event-names', type=str, dest='event_names', metavar='EVENT_NAMES_JSON',
-                        help='path to the JSON file with event names (required by --dump-scanner-configs and --generate-pnach)')
-    parser.add_argument('--scanner-configs', type=str, dest='scanner_configs', metavar='SCANNER_CONFIGS_JSON',
-                        help='path to the JSON file with scanner configs (required by --generate-pnach)')
+    subparsers = parser.add_subparsers(required=True)
 
-    parser.add_argument('--dump-event-names', type=str, dest='dump_event_names', metavar='EVENT_NAMES_JSON',
-                        help='dump event names to a JSON file')
-    parser.add_argument('--dump-scanner-configs', type=str, dest='dump_scanner_configs', metavar='SCANNER_CONFIGS_JSON',
-                        help='dump scanner configs to a JSON file')
-    parser.add_argument('--generate-pnach', type=str, dest='generate_pnach', metavar='PNACH_PATH',
-                        help='generate a patch file with new scanner configs')
+    elf_args = argparse.ArgumentParser(add_help=False)
+    elf_args.add_argument('--elf', type=str, help='path to game ELF (required)', required=True)
+
+    event_name_args = argparse.ArgumentParser(add_help=False)
+    event_name_args.add_argument('--event-names', type=str,
+                        help='path to the JSON file with event names (required)', required=True)
+
+    event_names = subparsers.add_parser('event-names')
+    subparser_event_names = event_names.add_subparsers(required=True)
+
+    parser_dump_event_names = subparser_event_names.add_parser('dump', help='dump the event names to a JSON file', parents=[elf_args])
+    parser_dump_event_names.add_argument('output_file', help='output file')
+    parser_dump_event_names.set_defaults(func=lambda parser, args: dumpEventNames(parser, args.output_file))
+
+    scanner_configs = subparsers.add_parser('scanner-configs')
+    subparser_scanner_configs = scanner_configs.add_subparsers(required=True)
+
+    parser_dump_scanner_configs = subparser_scanner_configs.add_parser('dump', help='dump the scanner configs to a JSON file', parents=[elf_args, event_name_args])
+    parser_dump_scanner_configs.add_argument('output_file', help='output file')
+    parser_dump_scanner_configs.set_defaults(func=lambda parser, args: dumpScannerConfigs(parser, args.event_names, args.output_file))
+
+    parser_patch_scanner_configs = subparser_scanner_configs.add_parser('patch', help='generate a patch file with new scanner configs',
+                                                        parents=[elf_args, event_name_args])
+    parser_patch_scanner_configs.add_argument('input_file', help='input file')
+    parser_patch_scanner_configs.add_argument('output_file', help='output file')
+    parser_patch_scanner_configs.set_defaults(func=lambda parser, args: patchScannerConfigs(parser, args.event_names, args.input_file, args.output_file))
+
+    button_action_hashes = subparsers.add_parser('button-action-hashes')
+    subparser_button_action_hashes = button_action_hashes.add_subparsers(required=True)
+
+    parser_dump_button_action_hashes = subparser_button_action_hashes.add_parser('dump', help='dump the button action hashes list to a JSON file', parents=[elf_args])
+    parser_dump_button_action_hashes.add_argument('output_file', help='output file')
+    parser_dump_button_action_hashes.add_argument('-n', '--num', type=int, help='number of hashes to dump (required)', required=True)
+    parser_dump_button_action_hashes.set_defaults(func=lambda parser, args: dumpButtonActionHashes(parser, args.num, args.output_file))
+
+    parser_patch_button_action_hashes = subparser_button_action_hashes.add_parser('patch', help='generate a patch file with new button action hashes', parents=[elf_args])
+    parser_patch_button_action_hashes.add_argument('input_file', help='input file')
+    parser_patch_button_action_hashes.add_argument('output_file', help='output file')
+    parser_patch_button_action_hashes.set_defaults(func=lambda parser, args: patchButtonActionHashes(parser, args.input_file, args.output_file))
 
     args = parser.parse_args()
-
-    if args.dump_event_names:
-        dumpEventNames(args.elf, args.dump_event_names)
-
-    if args.dump_scanner_configs:
-        dumpScannerConfigs(args.elf, args.event_names, args.dump_scanner_configs)
-
-    if args.generate_pnach:
-        generatePnachFile(args.elf, args.event_names, args.scanner_configs, args.generate_pnach)
+    with open(args.elf, 'rb') as f:
+        with mmap.mmap(f.fileno(), 0, None, mmap.ACCESS_READ) as elf:
+            parser = getParserForElf(elf)
+            parser.elf = elf
+            args.func(parser, args)
